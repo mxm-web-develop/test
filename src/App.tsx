9a589a00f1ec73b54e1703f3cf2afffdcb6edb06
mxm-web-develop <mxm.web.develop@gmail.com>
2023-11-22 00:55:41 +0800

    merge

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+   
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <h1>
+         this is an test for auto deploy
+        </h1>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
